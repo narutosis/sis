ae58e7a9c4753eab2840068d2158c486fe39459c
Degtiarev_Viktor <umkaquark.gmail.com>
2023-03-13 02:57:04 +0300

    дима писька6

--- laba.py
+++ laba.py
@@ -8,21 +8,21 @@ method = input()
 def InegralSum(funk, leftRange, rightRange, n, method):
     sum = 0
     if method == 1:
+        funk.replace("x", 'i * (rightRange -  leftRange) / n')
         for i in range(n):
-            sum += eval(funk.replace("x", 'i * (rightRange -  leftRange) / n')) * ((int(rightRange) - int(leftRange)) / n)
-            funk.replace('i * (rightRange -  leftRange) / n', "x")
+            sum += eval(funk) * ((int(rightRange) - int(leftRange)) / n)
     elif method == 2:
+        funk.replace("x", '(i + 1) * (rightRange -  leftRange) / n')
         for i in range(n):
-            sum += eval(funk.replace("x", '(i + 1) * (rightRange -  leftRange) / n')) * ((int(rightRange) - int(leftRange)) / n)
-            funk.replace('(i + 1) * (rightRange -  leftRange) / n', "x")
+            sum += eval(funk) * ((int(rightRange) - int(leftRange)) / n)
     elif method == 3:
+        funk.replace("x", '(i + 0.5) * (rightRange -  leftRange) / n')
         for i in range(n):
-            sum += eval(funk.replace("x", '(i + 0.5) * (rightRange -  leftRange) / n')) * ((int(rightRange) - int(leftRange)) / n)
-            funk.replace('(i + 0.5) * (rightRange -  leftRange) / n', "x")
+            sum += eval(funk) * ((int(rightRange) - int(leftRange)) / n)
     elif method == 4:
+        funk.replace("x", "(random.random() + 1) * (rightRange -  leftRange) / n")
         for i in range(n):
-            sum += eval(funk.replace("x", "(random.random() + 1) * (rightRange -  leftRange) / n")) * ((int(rightRange) - int(leftRange)) / n)
-            funk.replace("(random.random() + 1) * (rightRange -  leftRange) / n", "x")
+            sum += eval(funk) * ((int(rightRange) - int(leftRange)) / n)
     return sum
 
 

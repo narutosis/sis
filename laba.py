085f0854f26874a2a8e55f6df3eba6b368f2baeb
Degtiarev_Viktor <umkaquark.gmail.com>
2023-03-13 02:31:25 +0300

    дима писька3

--- laba.py
+++ laba.py
@@ -1,3 +1,5 @@
+from main import fun
+
 funk = input()
 leftRange = input()
 rightRange = input()
@@ -7,20 +9,20 @@ def InegralSum(funk, leftRange, rightRange, n, method):
     sun =0
     if method==1:
         for i in range(n):
-            sum+= eval(fun.replace("x", 'i * t[1] / n')) * (t[1] / n)
-            fun.replace('i * t[1] / n', "x")
+            sum+= eval(funk.replace("x", 'i * rightRange -  leftRange / n')) * (rightRange -  leftRange / n)
+            fun.replace('i * rightRange -  leftRange / n', "x")
     elif method==2:
         for i in range(n):
-            sum+= eval(fun.replace("x", '(i + 1) * t[1] / n')) * (t[1] / n)
-            fun.replace('(i + 1) * t[1] / n', "x")
+            sum+= eval(funk.replace("x", '(i + 1) * rightRange -  leftRange / n')) * (rightRange -  leftRange / n)
+            fun.replace('(i + 1) * rightRange -  leftRange / n', "x")
     elif method==3:
         for i in range(n):
-            sum+=eval(fun.replace("x", '(i + 0.5) * t[1] / n')) * (t[1] / n)
-            fun.replace('(i + 0.5) * t[1] / n', "x")
+            sum+=eval(funk.replace("x", '(i + 0.5) * rightRange -  leftRange / n')) * (rightRange -  leftRange / n)
+            fun.replace('(i + 0.5) * rightRange -  leftRange / n', "x")
     elif method==4:
         for i in range(n):
-            sum+=eval(fun.replace("x", "random.random() + 1) * t[1] / n")) * (t[1] / n)
-            fun.replace("random.random() + 1) * t[1] / n", "x")
+            sum+=eval(funk.replace("x", "random.random() + 1) * rightRange -  leftRange / n")) * (rightRange -  leftRange / n)
+            fun.replace("random.random() + 1) * rightRange -  leftRange / n", "x")
     return sum
 
 print(InegralSum(funk, leftRange, rightRange, n, method))

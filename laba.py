7ed9f48c7d061d8a1b110aba59ebe3b4279f332e
naruto_sis <dima89189687153@gmail.com>
2023-03-13 02:28:45 +0300

    lelfd

--- laba.py
+++ laba.py
@@ -7,16 +7,20 @@ def InegralSum(funk, leftRange, rightRange, n, method):
     sun =0
     if method==1:
         for i in range(n):
-            sum+=
+            sum+= eval(fun.replace("x", 'i * t[1] / n')) * (t[1] / n)
+            fun.replace('i * t[1] / n', "x")
     elif method==2:
         for i in range(n):
-            sum+=
+            sum+= eval(fun.replace("x", '(i + 1) * t[1] / n')) * (t[1] / n)
+            fun.replace('(i + 1) * t[1] / n', "x")
     elif method==3:
         for i in range(n):
-            sum+=
+            sum+=eval(fun.replace("x", '(i + 0.5) * t[1] / n')) * (t[1] / n)
+            fun.replace('(i + 0.5) * t[1] / n', "x")
     elif method==4:
         for i in range(n):
-            sum+=
+            sum+=eval(fun.replace("x", "random.random() + 1) * t[1] / n")) * (t[1] / n)
+            fun.replace("random.random() + 1) * t[1] / n", "x")
     return sum
 
 print(InegralSum(funk, leftRange, rightRange, n, method))
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import math
 import random
 
-
+fun = input()
 
 t = list(map(int, input().split()))
 n = int(input())
@@ -13,17 +13,20 @@ sum2 = 0
 sum3 = 0
 
 for i in range(n):
-    sum += math.cos(i * t[1] / n) * (t[1] / n)
+    sum += eval(fun.replace("x", 'i * t[1] / n')) * (t[1] / n)
+    fun.replace('i * t[1] / n', "x")
 
 for i in range(n):
-    sum1 += math.cos((i + 1) * t[1] / n) * (t[1] / n)
+    sum1 += eval(fun.replace("x", '(i + 1) * t[1] / n')) * (t[1] / n)
+    fun.replace('(i + 1) * t[1] / n', "x")
 
 for i in range(n):
-    sum2 += math.cos((i + 0.5) * t[1] / n) * (t[1] / n)
+    sum2 += eval(fun.replace("x", '(i + 0.5) * t[1] / n')) * (t[1] / n)
+    fun.replace('(i + 0.5) * t[1] / n', "x")
 
 for i in range(n):
-    sum3 += math.cos((i + random.random()) * t[1] / n) * (t[1] / n)
-
+    sum3 += eval(fun.replace("x", "random.random() + 1) * t[1] / n")) * (t[1] / n)
+    fun.replace("random.random() + 1) * t[1] / n", "x")
 
 print(math.sin(4))
 print(round(sum, 9), round(sum1, 9), round(sum2, 9), round(sum3, 9), sep='\n')
